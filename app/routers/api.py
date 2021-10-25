36c214d89e05c77be25fdebb17cf78d279607d9e
Suhrob <oopanndaa@gmail.com>
2021-10-25 21:30:31 +0500

    python version

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from . import telegram
+from . import telegram, authentication
 
 router = APIRouter()
 
 router.include_router(telegram.router)
+router.include_router(authentication.router, prefix='/login')
